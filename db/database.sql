a29227396689933d547434198b48bc42b83eceb3
Tyler <sta14031@byui.edu>
2019-06-29 14:15:58 -0600

    Got basic view posts functionality working

--- db/database.sql
+++ db/database.sql
@@ -7,4 +7,50 @@ CREATE TABLE Users
     PRIMARY KEY (Id)
 );
 
+CREATE TABLE Posts
+(
+    Id SERIAL,
+    Poster INT,
+    Title VARCHAR(32),
+    Content TEXT,
+    IsLink BOOLEAN,
+
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Poster) REFERENCES Users(Id)
+);
+
+CREATE TABLE Comments
+(
+    Id SERIAL,
+    Post INT,
+    Poster INT,
+    Content TEXT,
+
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Poster) REFERENCES Users(Id),
+    FOREIGN KEY (Post) REFERENCES Posts(Id)
+);
+
+CREATE TABLE Ratings
+(
+    Id SERIAL,
+    Post INT,
+    Val INT,
+
+    PRIMARY KEY (Id),
+    FOREIGN KEY (Post) REFERENCES Posts(Id)
+);
+
 INSERT INTO Users (Username) VALUES ('Tyler');
+
+INSERT INTO Posts
+    (Poster, Title, Content, IsLink)
+VALUES
+    (2, 'My test post',
+    'This is my awesome text post. It''s great!', FALSE);
+
+INSERT INTO Posts
+    (Poster, Title, Content, IsLink)
+VALUES
+    (2, 'My awesome link post',
+    'google.com/', TRUE);
--- server.js
+++ server.js
@@ -22,6 +22,19 @@ app.set('view engine', 'ejs')
 // The root
 app.get('/', (req, res) => res.render('public/index.html'))
 
+// The main page
+app.get('/test', (req, res) => {
+  res.render('pages/view_posts')
+})
+
+// Get all posts utility
+app.post('/getPosts', (req, res) => {
+  pool.query('SELECT * FROM Posts', (err, qres) => {
+    res.send(JSON.stringify(qres.rows))
+    res.end()
+  })
+})
+
 // Some tests
 app.get('/good', (req, res) => res.send('<h1>Good</h1>'))
 app.get('/bad', (req, res) => res.send('<h1>Bad</h1>'))
@@ -66,60 +79,6 @@ app.post('/log_in', (req, res) => {
           res.redirect('/bad')
         }
       })
-    }
-  )
-})
-
-// Test the DB query
-app.get('/test', (req, res) => {
-  pool.query('SELECT * FROM Users', (err, qres) => {
-    if (err) throw err;
-    res.send(JSON.stringify(qres.rows[0]))
-  })
-})
-
-// Old code
-app.get('/getPerson', (req, res) => {
-  let personID = req.query.id
-
-  pool.query(
-    'SELECT * FROM Person WHERE Id = $1',
-    [personID],
-    (err, qres) => {
-      if (err) throw err;
-      // console.log("User: ", qres.rows[0])
-      res.send(JSON.stringify(qres.rows[0]))
-      res.end()
-    }        
-  )
-})
-
-// Old code
-app.get('/getParents', (req, res) => {
-  let personID = req.query.id
-
-  pool.query(
-    'SELECT * FROM Person p INNER JOIN Parentage pa ON p.Id = pa.Father OR p.Id = pa.Mother WHERE Child = $1',
-    [personID],
-    (err, qres) => {
-      if (err) throw err;
-      res.send(JSON.stringify(qres.rows))
-      res.end()
-    }
-  )
-})
-
-// Old code
-app.get('/getChildren', (req, res) => {
-  let personID = req.query.id
-
-  pool.query(
-    'SELECT * FROM Person p INNER JOIN Parentage pa ON p.Id = pa.Child WHERE pa.Father = $1 OR pa.Mother = $1',
-    [personID],
-    (err, qres) => {
-      if (err) throw err;
-      res.send(JSON.stringify(qres.rows))
-      res.end()
     }
   )
 })
